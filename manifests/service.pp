64b367262123912274cfbc51c6f19d451b97ae83
Michael G. Noll <mnoll@verisign.com>
2013-09-26 10:39:11 +0200

    Quote strings

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,5 @@
 class diamond::service {
-  $ensure = $diamond::start ? { true => running, default => stopped }
+  $ensure = $diamond::start ? { true => 'running', default => 'stopped' }
   service { $diamond::service_name:
     ensure     => $ensure,
     enable     => $diamond::enable,
