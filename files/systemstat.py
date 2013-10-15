4ccc9e4394d12b1b9d44bba93ab58978fd0c677b
Michael G. Noll <mnoll@verisign.com>
2013-10-15 10:57:43 +0200

    Reformat systemstat.py

--- files/systemstat.py
+++ files/systemstat.py
@@ -13,52 +13,45 @@ import diamond.collector
 import os
 import re
 
-
 class SystemStatCollector(diamond.collector.Collector):
 
-    PROC = '/proc/stat'
-    MAX_VALUES = {
-        'ctxt': diamond.collector.MAX_COUNTER,
-    }
+  PROC = '/proc/stat'
+  MAX_VALUES = {
+    'ctxt': diamond.collector.MAX_COUNTER,
+  }
 
-    def get_default_config_help(self):
-        config_help = super(SystemStatCollector, self).get_default_config_help()
-        config_help.update({
-        })
-        return config_help
+  def get_default_config_help(self):
+    config_help = super(SystemStatCollector, self).get_default_config_help()
+    config_help.update({
+    })
+    return config_help
 
-    def get_default_config(self):
-        """
-        Returns the default collector settings
-        """
-        config = super(SystemStatCollector, self).get_default_config()
-        config.update({
-            'enabled':  'True',
-            'path':     'stat'
-        })
-        return config
+  def get_default_config(self):
+    """
+    Returns the default collector settings
+    """
+    config = super(SystemStatCollector, self).get_default_config()
+    config.update({
+      'enabled':  'True',
+      'path':     'stat'
+    })
+    return config
 
-    def collect(self):
-        if not os.access(self.PROC, os.R_OK):
-            return None
+  def collect(self):
+    if not os.access(self.PROC, os.R_OK):
+      return None
 
-        results = {}
-        # open file
-        file = open(self.PROC)
-        exp = '^(ctxt)\s(\d+)'
-        reg = re.compile(exp)
-        # Build regex
-        for line in file:
-            match = reg.match(line)
-            if match:
-                name = match.group(1)
-                value = match.group(2)
-                results[name] = self.derivative(name,
-                                                int(value),
-                                                self.MAX_VALUES[name])
+    results = {}
+    reg = re.compile('^(ctxt)\s(\d+)')
 
-        # Close file
-        file.close()
+    file = open(self.PROC)
+    for line in file:
+      match = reg.match(line)
+      if match:
+        name = match.group(1)
+        value = match.group(2)
+        results[name] = self.derivative(name, int(value), self.MAX_VALUES[name])
+    file.close()
 
-        for key, value in results.items():
-            self.publish(key, value, 2)
+    for key, value in results.items():
+      self.publish(key, value, 2)
